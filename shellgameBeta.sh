731bd6d8c81aa3ed3da35622a3a8cecf4150aae9
lapis999 <43965469+lapis999@users.noreply.github.com>
2018-10-08 13:36:43 -0500

    Update shellgameBeta.sh

--- shellgameBeta.sh
+++ shellgameBeta.sh
@@ -9,4 +9,8 @@ echo a simple CLI game built with bash
 echo "this game is not yet in a working state please do not use it"
 echo bash king is licensed under the gpl3 license
 
+if [[ $INPUT_STRING -eq m ]]
+then expr $starting_money + 2
+echo $staring_money
+
 
