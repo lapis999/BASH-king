4d011ff02e5841866aed67d2000288969a8b15a8
lapis999 <43965469+lapis999@users.noreply.github.com>
2018-10-08 12:26:35 -0500

    the game script
    to run drag into your terminal emulator or by using the sh BASH command.
    currently it only contains a little bit of info about BASH king.

---
+++ shellgameBeta.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+#starting bit
+version=0.01
+starting_money=50
+echo this is BASH KING version  $version
+m="m"
+echo a simple CLI game built with bash 
+echo "this game is not yet in a working state please do not use it"
+echo bash king is licensed under the gpl3 license
+
+
